8063d09f9f8586dfbb39eee9834ea80dcef72716
Kamil <drootnar@gmail.com>
2014-04-07 15:48:30 +0200

    Naprawa polskich znaków (kodowanie u góry pliku)

--- Class/File.py
+++ Class/File.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
+
+import nltk   
+import StringIO
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import urllib
 import re                       # do wyrażeń regularnych
 from hashlib import sha224      # do tworzenia hashy
-
 class File():
     def __init__(self,path):
         self.pathToFile=path
@@ -18,7 +22,7 @@ class File():
     def PrepareSentenceToHash(self,sentence):
         ''' #Input: string sentence (unprocessed) 
         Output: string sentence (processed)
-        Description: Fuction makes lower cases, deletes all punctuations (all spaces as well), polish joins-word (i, ale, pod) and changes polish letter to english equivalent.
+        Description: Function makes lower cases, deletes all punctuations (all spaces as well), polish joins-word (i, ale, pod) and changes polish letter to english equivalent.
         '''
         
         sentence = "Australijski okręt biorący udział w poszukiwaniach samolotu malezyjskich linii lotniczych wykrył sygnały podobne do tych, jakie emitują czarne skrzynki - poinformowały w poniedziałek władze w Canberze."
@@ -60,16 +64,58 @@ class File():
     
     '''Method take HTML code and delete from it tags, operators etc. At end we have clear text'''
     def ParseHTML(self,HTMLtext):
-        '''TODO:RAFAL'''
-        parsedText=""
+        #TODO:RAFAL
+        parsedText = nltk.clean_html(HTMLtext)  
         return parsedText
     
     '''Method get path and return big text from file'''
     def LoadTextFromFile(self,path):
         '''TODO: RAFAL '''
-        text=''
+        if(path.find('.pdf')>=0):
+            text=self.LoadFromPdf(path)
+        if(path.find('.txt')>=0):
+            text=self.LoadFromTxt(path)
+        if(path.find('.html')>=0):
+            text=self.LoadFromHtml(path)      
         return text
     
+    '''download file'''
+    def Download(self,path):
+        #TODO:RAFAL
+        usock = urllib.urlopen(path)
+        text=usock.read()
+        usock.close()
+        return text
+
+    ''' Return text from .html'''
+    def LoadFromHtml(self,path):
+        #TODO:RAFAL
+        HTMLtext=self.LoadFromTxt(path)
+        text=self.ParseHTML(HTMLtext)
+        return text
+    
+    ''' Return text from .txt'''
+    def LoadFromTxt(self,path):
+        #TODO:RAFAL
+        f = open(path, 'r')
+        HTMLtext=f.read()
+        f.close()
+        return text
+    
+    ''' Return text from .pdf'''
+    def LoadFromPdf(self,path):
+        #TODO:RAFAL
+        text = ""
+        # Load PDF into pyPDF
+        pdf = PdfFileReader(open("path", "rb"))
+        # Iterate pages
+        for i in range(0, pdf.getNumPages()):
+            # Extract text from page and add to content
+            print i
+            text += pdf.getPage(i).extractText() + "\n"
+        
+        return text
+
     '''Return true if contain www or http'''
     def IsLink(self,sentence):
         '''
