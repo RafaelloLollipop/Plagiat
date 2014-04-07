7fdcc11190a35b4e4aae5852040b7af1ff221351
Kamil <drootnar@gmail.com>
2014-04-07 19:16:01 +0200

    Stworzyłem metodę MakeHashedText - dzieli tekst na zdania. Metodologię opisałem w __doc__. TO DO: Dokończyć pozostałe wywoływane funkcje składowe (narazie się zakomentowane, by nie wywalać błędów).

--- Class/OutFile.py
+++ Class/OutFile.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import sys
+import re
 from os import path
 sys.path.append(path.abspath('./Class'))
 from File import *
@@ -45,19 +46,42 @@ class OutFile(File):
         At end we get hashedText List - List of senetence hash '''
     def MakeHashedText(self,text):
         '''
-        text(string) - clear text like " Rafal jest ok. Kamil spoko loko.
-        TODO:KAMIL
+        Input: tekst ze źródła, który ma zostać podzielony
+        Output: True, jeżeli proces poszedł dobrze (czyli aktualnie zawsze:P)
+        Description: użyłem następujących założeń: 1) Szukam substringa zaczynącego się od małej litery i zakończonego .!? Następnie sprawdzam czy ostatnie słowo nie jest skrótem np. mgr. prof. km. Jeżeli tak to to zdanie i następne należy połączyć. Jeżeli ostatnie słowo nie jest skrótem to to zdanie i następne należy rodzielić.
+        Nie ma problemu ze wielokrotnymi kropkami itp. ponieważ zostaną usuniętę w metodzie PrepareSectenceToHash
         '''
         
-        # Loop
-        sentence=""
-        sentencePrepared=self.PrepareSectenceToHash(sentence)
-        sentenceHash=self.HashSentence(sentencePrepared)
-        self.hashedText.append(sentenceHash)
-        # End loop
+        print text
+        query = "[A-Z]*[.?!] " # Zapytanie regex
+        temp=re.split(query ,text) # Wstępny podział tekstu
+    
+        lista = []
+        skrot = 0;
+        
+        for el in temp:
+            if (skrot==0): # sprawdzam czy to zdanie jest po skrócie
+                # zdanie nie jest po skrócie
+                if ((el[-2]==' ') or (el[-3]==' ') or (el[-4]==' ') or (el[-5]==' ')): # szukam skrótu
+                    lista.append(el)
+                    skrot = 1;
+                else:
+                    lista.append(el)
+            else:   # zdanie jest po skrócie
+                lista[-1] = lista[-1] + ". " + el # zatem dopisuję do ostatniego wpisu
+                if ((el[-2]==' ') or (el[-3]==' ') or (el[-4]==' ') or (el[-5]==' ')): # czy teraz na końcu znów nie ma skrótu?
+                    skrot = 1;
+                else:
+                    skrot = 0
+        
+        for sentence in lista:
+            print sentence
+            # metody jeszcze nie napisane, więc jeszcze komment
+            #sentencePrepared = self.PrepareSectenceToHash(sentence)
+            #sentenceHash=self.HashSentence(sentencePrepared)
+            #self.hashedText.append(sentenceHash)
         
         return True
-
 
 
     '''Method Download file and return path to it'''
