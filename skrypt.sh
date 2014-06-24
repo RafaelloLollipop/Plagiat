231fec4a8afad2822541fa3dfab8d6e322d3c4ec
Kamil <drootnar@gmail.com>
2014-06-24 15:32:00 +0200

    skypt basha ready to use ;)

---
+++ skrypt.sh
@@ -0,0 +1,10 @@
+!/bin/bash 
+git clone git://github.com/Vallher/Plagiat/
+cd Plagiat
+sudo apt-get install python
+sudo apt-get install python python-tk idle python-pmw python-imaging
+sudo apt-get install aaptitude
+sudo aptitude install python-qt4
+sudo pip install -U pyyaml nltk
+sudo pip install PyPDF2
+sudo pip install ngram
